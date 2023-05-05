70db98bf193e6444729654fdb58c6b6a502b8072
aneeshprasobhan <aneeshprasobhan@gmail.com>
2023-05-06 01:30:14 +0200

    Yaw Calibration working

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,10 @@ float pitch = 0.0F;
 float roll  = 0.0F;
 float yaw   = 0.0F;
 
+float gyroOffsetX = 0;
+float gyroOffsetY = 0;
+float gyroOffsetZ = 0;
+
 
 void setup() {
     M5.begin();             // Init M5StickC Plus.
@@ -26,12 +30,23 @@ void setup() {
     M5.Lcd.println("  X       Y       Z");
     M5.Lcd.setCursor(30, 70);
     M5.Lcd.println("  Pitch   Roll    Yaw");
+    //Setup Serial
+    Serial.begin(115200);
+
+    M5.IMU.CalibrateGyro(10);
+    M5.IMU.getCalibData(&gyroOffsetX, &gyroOffsetY, &gyroOffsetZ);  // Get gyro offsets after calibration
+
 }
 
 
 void loop() {
     static float temp = 0;
     M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+    
+    gyroX -= gyroOffsetX;  // Subtract gyro offset from gyro reading
+    gyroY -= gyroOffsetY;
+    gyroZ -= gyroOffsetZ;
+    
     M5.IMU.getAccelData(&accX, &accY, &accZ);
     M5.IMU.getAhrsData(&pitch, &roll, &yaw);
     M5.IMU.getTempData(&temp);
@@ -48,5 +63,22 @@ void loop() {
 
     M5.Lcd.setCursor(30, 95);
     M5.Lcd.printf("Temperature : %.2f C", temp);
+
+        // Print Gyro data
+    Serial.print("Gyro x,y,z: ");
+    Serial.print(gyroX);
+    Serial.print(", ");
+    Serial.print(gyroY);
+    Serial.print(", ");
+    Serial.println(gyroZ);
+
+    // Print Accel data
+    Serial.print("Acc x,y,z: ");
+    Serial.print(accX);
+    Serial.print(", ");
+    Serial.print(accY);
+    Serial.print(", ");
+    Serial.println(accZ);
+
     delay(50);
 }
